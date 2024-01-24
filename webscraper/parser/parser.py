c891f09b575f22c63ec5356709b7c4bd42361aa3
Kevin Locksy <nhan.kev@gmail.com>
2024-01-24 19:12:41 +0100

    Fixed event of the day retrieval and externalised global constant and start generating Event with Link, Type, date

--- webscraper/parser/parser.py
+++ webscraper/parser/parser.py
@@ -7,50 +7,77 @@ import re
 from bs4 import BeautifulSoup
 from dto.event.Event import Event
 
+PARSER_TYPE='html.parser'
+TAG_CONTENT='ici'
+TAG_DAY='ul'
+TAG_EVENT='li'
+
 listEvent = []
 
 def retrieveEventList(url):
   html=""
   try:
     response = urllib.request.urlopen(url).read()
-    html = BeautifulSoup(response,'html.parser')
-    #print(html.prettify())
-    content_tag='ici'
-    content = html.find(content_tag)
-    allelement = content.get_text(separator=';')
-    current_ul = content.find('ul')
-    last_ul = content.find_all("ul")[-1]
+    html = BeautifulSoup(response,PARSER_TYPE)
+    content = html.find(TAG_CONTENT)
+    currentDayEvents = content.find(TAG_DAY)
 
-    while current_ul != None:
-      date = current_ul.previousSibling
-      assert checkDate(date)
+    while currentDayEvents != None:
+      event_date = currentDayEvents.previousSibling.strip()
+      assert checkDate(event_date)
 
-      eventsOfTheDate = current_ul.find_all("li")
-      for e in eventsOfTheDate:
-        print("---- event -----")
-        event = Event()
-        #print(e.get_text(separator=';'))
-
-        link_list = e.find_all('a')
-        print(link_list)
-        event.setDate(date)
-        #event.setLink(link)
-        listEvent.append(event)
-      current_ul = current_ul.find_next_sibling('ul')
-    getDate()
-  except Exception as e: 
-    logging.error(f"An exception occurred while trying to scrape the url: {url}")
-    logging.debug(e,exc_info=True)
+      eventsOfTheDate = generateListDayEvent(currentDayEvents,TAG_EVENT,event_date)
+      break
+      currentDayEvents = currentDayEvents.find_next_sibling('ul')
+  except Exception as e:
+    # logging.error(f"An exception occurred while trying to scrape the url: {url}")
+    # logging.debug(e,exc_info=True)
+    print(e)
   return html
 
-def getDate(dom):
-  
-  return ""
+#generate the list of Event objects
+def generateListDayEvent(content,tagName,date):
+  raw_eventsOfTheDay = content.find_all(tagName)
+  TAG = '<'+tagName+'>'
+  eventsOfTheDay = []
+  while len(raw_eventsOfTheDay)>0:
+    raw_event = raw_eventsOfTheDay.pop()
+    event_str = str(raw_event)
+    if(int(event_str.count(TAG)) == 1): #checks if event list is well formed
+      event = BeautifulSoup(raw_event.decode_contents(), PARSER_TYPE)  #removes outer tag 'li'
+      #eventsOfTheDay.append(event)
+      generateEvent(date,event)
+      continue
+    else:
+      indices = [index for index in range(len(event_str)) if event_str.startswith(TAG, index)]
+      for i in range(len(indices)):
+        if i==len(indices)-1:
+          raw_event = event_str[indices[i]+len(TAG):]
+        else:
+          raw_event = event_str[indices[i]+len(TAG):indices[i+1]]
+        event = BeautifulSoup(raw_event, PARSER_TYPE) #reformate event tag li and removes 'li' tag
+        generateEvent(date,event)
+        # eventsOfTheDay.append(event)
+  return eventsOfTheDay
 
-def formatDate(date):
+def generateEvent(date,bs4Tag):
+  print(bs4Tag)
+  event = Event()
+
+  food_str = ""
+  print(bs4Tag.get_text())
+  if re.search('Restauration', bs4Tag.get_text(), re.IGNORECASE):
+    print("ok") 
+  event_link = bs4Tag.find('a')['href']
+  event_type = re.sub(r'[()]', '', bs4Tag.i.string) #removes ()
+
+  event.setLink(event_link) #check if multiple 'a'
+  event.setType(event_type)
+  event.setDate(date)
+  print(event)
   return
 
-def getAddress(dom):
+def formatDate(date):
   return
 
 def checkDate(text):
@@ -58,7 +85,3 @@ def checkDate(text):
   if (re.search(regex,text)):
     return True
   return False
-
-def checkNewEvent(text):
-  
-  return 
